d7ac51b2aeb0598785a44e5caa1287d59882ee6f
jessicaouyang <jouyang99@gmail.com>
2022-03-26 11:41:09 -0500

    new react native app

--- iot-rfid/src/App.js
+++ iot-rfid/src/App.js
@@ -1,5 +1,25 @@
+import React from 'react';
+import {
+  View,
+  Text, 
+  StyleSheet,
+} from 'react-native';
+
+
 import logo from './logo.svg';
 import './App.css';
+
+const NewApp = () => {
+  return (
+  <View>
+    <Text>KeyDrop</Text>
+    <View>
+      <Text style={style.tap}>Log In</Text>
+    </View>
+  </View>
+
+  )
+}
 
 function App() {
   return (
@@ -7,10 +27,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jessica is so good at coding
         </p>
         <a
-          className="App-link"
+          className="App-link" 
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
