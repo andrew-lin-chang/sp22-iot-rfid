3c795024dd05e39e8d9e0ca204163aea0d73dfb9
anushkashah <anushkashah654@gmail.com>
2022-03-29 19:53:48 -0500

    reverted back

--- rfid/screens/TabOneScreen.tsx
+++ rfid/screens/TabOneScreen.tsx
@@ -9,7 +9,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     <View style={styles.container}>
       <Text style={styles.title}>Digital Wallet</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOne.tsx" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
